720841e251dc00c15d81100e229fde5a23966d9d
Alex Arena <im@alexarena.com>
2017-03-11 17:44:46 -0700

    filled out test a bit more

--- tests/routes-test.js
+++ tests/routes-test.js
@@ -1,9 +1,21 @@
 let chai = require('chai')
 let expect = chai.expect
 
+let app = require('../index')
+let request = require('request')
+
+// There's only one (very incomplete) test for all routes.
+// You can try writing the rest for practice.
+// If you aren't sure where to start, take a look at todo-test.js
+
 describe('GET /', ()=>{
   it('should respond with status 200',(done)=>{
-    expect(200).to.equal(200)
-    done()
+    request.get('http://localhost:3000/',(err,res,body)=>{
+      expect(res.statusCode).to.equal(200)
+
+      //Because this is async, we need to use the done() method.
+      //Learn more about async unit tests in Mocha here: http://staxmanade.com/2015/11/testing-asyncronous-code-with-mochajs-and-es7-async-await/
+      done()
+    })
   })
 })
